1a1521cb3de62dafb68a24e43125ade9aa8699ce
Mauricio Yamanga <myamanga@asjservicios.com>
2024-07-25 16:24:19 -0300

    infoDetails en el Controller y infoDetails.json

--- src/main/java/com/example/fidely/controllers/Rest.java
+++ src/main/java/com/example/fidely/controllers/Rest.java
@@ -4,6 +4,8 @@ package com.example.fidely.controllers;
 import com.example.fidely.service.JsonHandler;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
+
 
 @RestController
 @RequestMapping("/fidelyMock")
@@ -18,6 +20,13 @@ public class Rest {
     @GetMapping("/login")
     public Object login() {
         return JsonHandler.readJson("loginResponse.json");
+    }
+
+    @GetMapping("/infoDetails")
+    public Object infoDetails() {
+        String metodo = "infoDetailsController";
+        System.out.println(metodo + "Data :"+JsonHandler.readJson("infoDetails.json") +" "+ LocalTime.now());
+        return JsonHandler.readJson("infoDetails.json");
     }
 
 }
