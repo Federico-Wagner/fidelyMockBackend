f9478905465cc236a19b5dff5d552b7f5347ea9a
fede0104 <federico.r.wagner@gmail.com>
2024-07-25 11:04:52 -0300

    backend-start

---
+++ src/main/java/com/example/fidely/controllers/Rest.java
@@ -0,0 +1,22 @@
+package com.example.fidely.controllers;
+
+
+import com.example.fidely.service.JsonHandler;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/fidelyMock")
+public class Rest {
+
+    @GetMapping("/get")
+    public String get() {
+        return "hola mundo";
+    }
+
+    @GetMapping("/login")
+    public Object login() {
+        return JsonHandler.readJson("loginResponse.json");
+    }
+
+}
---
+++ src/main/java/com/example/fidely/service/JsonHandler.java
@@ -0,0 +1,24 @@
+package com.example.fidely.service;
+
+
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
+import java.io.*;
+
+
+public class JsonHandler {
+
+    public static Object readJson(String jsonFileName) {
+        Object object;
+        try{
+        FileReader fileReader = new FileReader("src/main/resources/responses/" + jsonFileName);
+        object = new JSONParser(fileReader).parse();
+        }catch (FileNotFoundException fne){
+            object = "Error, archivo no encontrado";
+        }catch (ParseException pe){
+            object = "Error, fallo al parsear";
+        }
+        return object;
+    }
+
+}
