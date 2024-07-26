ba106ab191a5131befea32026ae96ac147e60256
Mauricio Yamanga <myamanga@asjservicios.com>
2024-07-26 19:48:07 -0300

    Controller /consumes

--- src/main/java/com/example/fidely/controllers/Rest.java
+++ src/main/java/com/example/fidely/controllers/Rest.java
@@ -2,6 +2,7 @@ package com.example.fidely.controllers;
 
 
 import com.example.fidely.service.JsonHandler;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
@@ -27,6 +28,13 @@ public class Rest {
         String metodo = "infoDetailsController";
         System.out.println(metodo + "Data :"+JsonHandler.readJson("infoDetails.json") +" "+ LocalTime.now());
         return JsonHandler.readJson("infoDetails.json");
+    }
+
+    @GetMapping("/consumes")
+    public ResponseEntity<Object> consumes() {
+        String metodo = "infoDetailsController";
+        System.out.println(metodo + "Data :"+JsonHandler.readJson("consume.json") + " "+ LocalTime.now());
+        return ResponseEntity.ok(JsonHandler.readJson("consume.json"));
     }
 
 }
