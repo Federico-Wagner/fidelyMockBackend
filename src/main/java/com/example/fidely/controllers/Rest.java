424eb2012fde6c5d13a8b2ebada337f12a956e95
fede0104 <federico.r.wagner@gmail.com>
2024-07-30 16:23:48 -0300

    estandarizacion log

--- src/main/java/com/example/fidely/controllers/Rest.java
+++ src/main/java/com/example/fidely/controllers/Rest.java
@@ -18,35 +18,35 @@ public class Rest {
     @GetMapping("/login")
     public Object login() {
         Object response = JsonHandler.readJson("loginResponse.json");
-        System.out.println("/login - response: "+ response);
+        System.out.println("/login - response: " + response);
         return response;
     }
 
     @GetMapping("/profile")
     public Object profile() {
         Object response = JsonHandler.readJson("profileResponse.json");
-        System.out.println("/profile - response: "+ response);
+        System.out.println("/profile - response: " + response);
         return response;
     }
 
     @GetMapping("/brandColors")
     public Object brandColors() {
         Object response = JsonHandler.readJson("brandColorsResponse.json");
-        System.out.println("/brandColors - response: "+ response);
+        System.out.println("/brandColors - response: " + response);
         return response;
     }
 
     @GetMapping("/infoDetails")
     public Object infoDetails() {
         Object response = JsonHandler.readJson("infoDetails.json");
-        System.out.println("/infoDetails - response: "+ response);
+        System.out.println("/infoDetails - response: " + response);
         return response;
     }
 
     @GetMapping("/consumes")
     public Object consumes() {
         Object response = JsonHandler.readJson("consume.json");
-        System.out.println("/consumes - response: "+ response);
+        System.out.println("/consumes - response: " + response);
         return response;
     }
 
@@ -54,7 +54,7 @@ public class Rest {
     public Object imageA() {
         String imageName = "banner11.png";
         Object response = ImageHandler.readImage(imageName);
-        System.out.println("/images/A - response: "+ imageName);
+        System.out.println("/images/A - response: " + imageName);
         return response;
     }
 }
